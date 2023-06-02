c96f640322e00cbd55687d2ba5eb332c824ac83d
Rivaldes Tiyo <tiyorivaldes@gmail.com>
2023-06-02 14:15:35 +0100

    hjjhjh

--- src/app/guard/auth.guard.ts
+++ src/app/guard/auth.guard.ts
@@ -14,22 +14,25 @@ export class AuthGuard implements CanActivate {
     private router: Router,
     private authService: AuthenticationService,
     private alertService: AlertService
-    ) { 
-      this.alert = alertService;
-    }
+  ) {
+    this.alert = alertService;
+  }
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    
-    if (this.authService.isLogged()) {
-      // logged in so return true
+    if (navigator.onLine) {
+      if (this.authService.isLogged()) {
+        // logged in so return true
+        return true;
+      }
+
+      // not logged in so redirect to login page with the return url
+      this.alertService.warning("Vous devez être connecté avant d'accéder à cette page!");
+      this.router.navigate(['/login'], { queryParams: { returnUrl: state.url, action: true } });
+      return false;
+    } else {
       return true;
     }
-
-    // not logged in so redirect to login page with the return url
-    this.alertService.warning("Vous devez être connecté avant d'accéder à cette page!");
-    this.router.navigate(['/login'], { queryParams: { returnUrl: state.url, action:true } });
-    return false;
   }
 }
--- src/app/guard/privilege.guard.ts
+++ src/app/guard/privilege.guard.ts
@@ -6,33 +6,37 @@ import { AlertService } from "../services/alert.service";
 import * as CONST from "../app-const";
 
 @Injectable({
-	providedIn: "root"
+  providedIn: "root"
 })
 export class PrivilegeGuard implements CanActivate {
-	
-	constructor(
-		private authService: AuthenticationService,
-		private alertService: AlertService,
-		private router: Router
-	) {}
 
-	canActivate(
-		next: ActivatedRouteSnapshot,
-		state: RouterStateSnapshot
-	): Observable<boolean> | Promise<boolean> | boolean {
-		
-		let privilegeID = next.data["privilegeID"] as number;
-		sessionStorage.setItem(CONST.CURRENT_URL_SESSION_VARNAME, state.url)
+  constructor(
+    private authService: AuthenticationService,
+    private alertService: AlertService,
+    private router: Router
+  ) { }
 
-		if(!this.authService.isLogged()){
-			localStorage.setItem(CONST.CURRENT_URL_SESSION_VARNAME, state.url)
-			this.router.navigate(['logout']);
-		}
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> | Promise<boolean> | boolean {
 
-		return this.authService.hasPrivilege(privilegeID, () => {
-			this.alertService.warning("Désolé. Vous n'avez pas le privilège pour effectuer cette action !");
-			if (privilegeID === 1)
-				this.router.navigate(['/logout']);
-		});
-	}
+    let privilegeID = next.data["privilegeID"] as number;
+    sessionStorage.setItem(CONST.CURRENT_URL_SESSION_VARNAME, state.url)
+
+    if (navigator.onLine) {
+      if (!this.authService.isLogged()) {
+        localStorage.setItem(CONST.CURRENT_URL_SESSION_VARNAME, state.url)
+        this.router.navigate(['logout']);
+      }
+
+      return this.authService.hasPrivilege(privilegeID, () => {
+        this.alertService.warning("Désolé. Vous n'avez pas le privilège pour effectuer cette action !");
+        if (privilegeID === 1)
+          this.router.navigate(['/logout']);
+      });
+    } else {
+      return true;
+    }
+  }
 }
